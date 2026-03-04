4f7143729f061f2c83f7294870fcc3216aaa3d02
Nikita93 <nikitaaj39@gmail.com>
2026-03-04 14:07:03 -0500

    reverting ports for render

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 uvicorn api:app --host 0.0.0.0 --port 8001 &
 
 # Start Streamlit in foreground (replace shell)
-exec streamlit run app.py --server.port=7860 --server.address=0.0.0.0
+exec streamlit run app.py --server.port=8000 --server.address=0.0.0.0
